d78d7de67502a34a0931c41b25add335a39ea821
core@core1 <core@core1>
2014-12-12 13:00:04 +0900

    first commit

---
+++ files/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+REPO=docker-nginx-php-libmysql
+export URL_GIT=https://raw.githubusercontent.com/typista/$REPO/master/files
+LOCALTIME=/etc/localtime
+if [ ! -L $LOCALTIME ]; then
+	rm $LOCALTIME
+	ln -s /usr/share/zoneinfo/Asia/Tokyo $LOCALTIME
+fi
+EXEC1ST=/root/export/exec1st.sh
+if [ ! -f $EXEC1ST ];then
+	wget $URL_GIT/exec1st.sh -O $EXEC1ST
+fi
+if [ ! -x $EXEC1ST ];then
+	chmod +x $EXEC1ST
+fi
+$EXEC1ST
+/etc/init.d/crond start
+/usr/bin/tail -f /dev/null
